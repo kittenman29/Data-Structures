c815a866dc9ad61b9bd7636193c9ee86b0dee472
Will Schulz <will.schulz29@gmail.com>
2019-07-23 23:12:20 -0400

    queue done

--- queue/queue.py
+++ queue/queue.py
@@ -1,24 +1,48 @@
+# class Node:
+#   def __init__(self, value=None, next_node=None):
+#     # the value at this linked list node
+#     self.value = value
+#     # reference to the next node in the list
+#     self.next_node = next_node
+
+#   def get_value(self):
+#     return self.value
+
+#   def get_next(self):
+#     return self.next_node
+
+#   def set_next(self, new_next):
+#     # set this node's next_node reference to the passed in node
+#     self.next_node = new_next
+
 class Queue:
-  def __init__(self, size=0, storage=[]):
-    self.size = size
+  def __init__(self):
+    self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = storage
+    self.storage = []
 
   def enqueue(self, item):
     # pass
-    if item is None:
-      return None
-    else:
-      self.storage.append(item)
-    return len(self.size)
+    self.size += 1
+    self.storage.append(item)
+    return self.len()
+    # print(self.storage)
+
+    # return self.size
   
   def dequeue(self):
-    pass
-    # if len(self.size) < 1:
-    #   return None
-    # return self.storage.pop()
+    # pass
+    if self.len() < 1:
+      return None
+    else:
+      self.size -= 1
+      return self.storage.pop(0)
+      # return self.len()
 
   def len(self):
-    pass
-    # return len(self.size)
+    # pass
+    return len(self.storage)
+
+# new_queue = Queue()
+# print(new_queue.len())
